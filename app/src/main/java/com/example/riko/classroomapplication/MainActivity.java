a1fccdf106fb8b14e53ad1850148f154dcabe790
RIKO <Fujii.Y0501@gmail.com>
2018-04-26 23:54:56 +0700

    Delete test comment

--- app/src/main/java/com/example/riko/classroomapplication/MainActivity.java
+++ app/src/main/java/com/example/riko/classroomapplication/MainActivity.java
@@ -16,8 +16,6 @@ public class MainActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //This is my test code
-
         btnSignin = findViewById(R.id.btnSignin);
         btnSignup = findViewById(R.id.btnSignup);
 
