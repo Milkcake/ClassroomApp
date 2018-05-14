f6700ec52fce1b25af774b0daa85660088be83da
RIKO <Fujii.Y0501@gmail.com>
2018-05-15 02:57:25 +0700

    Back press MainActivity.java

--- app/src/main/java/com/example/riko/classroomapplication/MainActivity.java
+++ app/src/main/java/com/example/riko/classroomapplication/MainActivity.java
@@ -1,15 +1,19 @@
 package com.example.riko.classroomapplication;
 
 import android.content.Intent;
+import android.os.Handler;
+import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity{
 
     private Button btnSignin, btnSignup;
+    private boolean doubleBackToExitPressedOnce;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +52,30 @@ public class MainActivity extends AppCompatActivity{
                 startActivity(signUp);
             }
         });
+    }
 
+    //--------- Back Press --------------------***//
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_HOME);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+            System.exit(1);
+            return;
+        }
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler().postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
